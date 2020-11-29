fe43b0be93a26d5885156e9609be8e1c8ba60a38
nkneeraj <nkneerj641@gmail.com>
2020-11-29 10:52:05 +0530

    [NK]Added: Ability to remove contact from payroll table.

--- EmployeePayrollService/js/home.js
+++ EmployeePayrollService/js/home.js
@@ -1,62 +1,57 @@
+let employeePayrollList;
+
 window.addEventListener("DOMContentLoaded", (event) => {
+    employeePayrollList = getEmployeeFromStorage();
+    console.log(employeePayrollList);
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
+   createInnerHtml();
+});
+
+const getEmployeeFromStorage = () => {
+    return localStorage.getItem("EmployeePayrollList") ? JSON.parse(localStorage.getItem("EmployeePayrollList")) : [];
+};
+
+const createInnerHtml = () => {
+   const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
+   if(employeePayrollList.length == 0) return;
+   let innerHtml = `${headerHtml}`
+   for(const employee of employeePayrollList){
+   innerHtml = `${innerHtml}
+   <tr>
+   <td>
+       <img class="profile" alt="" src="${employee._picture}">
+   </td>
+   <td>${employee._name}</td>
+   <td>${employee._gender}</td>
+   <td>${getDeptHtml(employee._department)}</td>
+   <td>${employee._salary}</td>
+   <td>${stringifyDate(employee._startDate)}</td>
+   <td>
+       <img id="${employee._name}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+       <img id="${employee._id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+   </td>
+</tr>
+   `;
+}
+   document.querySelector("#display").innerHTML = innerHtml;
+};
+
+const remove = (node) => {
+    console.log(node.name);
+    let employee = employeePayrollList.find(emp => emp._name == node.id);
+    if(!employee) return;
+    const index = employeePayrollList.map(emp => emp._name)
+                                     .indexOf(employee._name);
+    employeePayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
- });
- 
- const createInnerHtml = () => {
-    const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
-    let innerHtml = `${headerHtml}`
-    let employeePayrollList = createEmployeePayrollJSON();
-    for(const employee of employeePayrollList){
-    innerHtml = `${innerHtml}
-    <tr>
-    <td>
-        <img class="profile" alt="" src="${employee._picture}">
-    </td>
-    <td>${employee._name}</td>
-    <td>${employee._gender}</td>
-    <td>${getDeptHtml(employee._department)}</td>
-    <td>${employee._salary}</td>
-    <td>${employee._startDate}</td>
-    <td>
-        <img id="${employee._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-        <img id="${employee._id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
-    </td>
- </tr>
-    `;
- }
-    document.querySelector("#display").innerHTML = innerHtml;
- };
- 
- const createEmployeePayrollJSON = () => {
-    let employeeListLocal = [
-        {
-        _name: "Neeraj",
-        _gender: "Male",
-        _department: ["finance"],
-        _salary: "80000",
-        _startDate: "19 Nov 2020",
-        _note: "",
-        _id: new Date().getTime(),
-        _picture: "../assets/profile-images/Ellipse -2.png"
-        },
-    {
-     _name: "Mehak",
-     _gender: "Male",
-     _department: ["finance","Sales"],
-     _salary: "60000",
-     _startDate: "19 Nov 2018",
-     _note: "",
-     _id: new Date().getTime() + 1,
-     _picture: "../assets/profile-images/Ellipse -3.png"
+};
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = "";
+    for(const dept of deptList){
+        deptHtml = `${deptHtml}<div class="dept-label">${dept}</div>`
     }
-    ];
-    return employeeListLocal;
- };
- 
- const getDeptHtml = (deptList) => {
-     let deptHtml = "";
-     for(const dept of deptList){
-         deptHtml = `${deptHtml}<div class="dept-label">${dept}</div>`
-     }
-     return deptHtml;
- };
+    return deptHtml;
+};
